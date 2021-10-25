d9f78279ae90f451ddbe5072c7c14bb2963dcb03
asen23 <andersenlim23@gmail.com>
2021-10-25 19:01:57 +0700

    add starter code

---
+++ App.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {SafeAreaView} from 'react-native';
+
+const App: () => Node = () => {
+  return <SafeAreaView></SafeAreaView>;
+};
+export default App;
