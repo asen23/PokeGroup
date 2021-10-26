6ee4abf9f6febf96d5511441e8f5fa2235f2acb8
asen23 <andersenlim23@gmail.com>
2021-10-26 22:09:52 +0700

    feat: add custom button

---
+++ src/components/Button.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+
+const Button = ({ title, backgroundColor, textColor }) => {
+    return (
+        <TouchableOpacity
+            style={[
+                styles.button,
+                backgroundColor ? { backgroundColor: backgroundColor } : null,
+            ]}>
+            <Text
+                style={[styles.text, textColor ? { color: textColor } : null]}>
+                {title}
+            </Text>
+        </TouchableOpacity>
+    );
+};
+
+export default Button;
+
+const styles = StyleSheet.create({
+    text: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        textAlign: 'center',
+    },
+    button: {
+        borderRadius: 5,
+    },
+});
