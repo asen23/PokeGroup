5e6d02df3db1c8f2ce56394208ad5839b194b175
asen23 <andersenlim23@gmail.com>
2021-10-26 22:11:13 +0700

    feat: update home

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,25 +1,46 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, ScrollView } from 'react-native';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import Card from '../../components/Card';
 import PokemonNoDetail from '../../components/PokemonNoDetail';
+import PokemonBasicDetail from '../../components/PokemonBasicDetail';
 import { addExample } from '../../redux/reducer/example';
+import PokemonFullDetail from '../../components/PokemonFullDetail';
+import Button from '../../components/Button'
 
 export default function Home() {
     const example = useSelector(state => state.example.example);
     const dispatch = useDispatch();
     return (
-        <View>
+        <ScrollView>
             <Card>
                 <Text>{example}</Text>
-                <Button title="add" onPress={() => dispatch(addExample())} />
+                <Button title="add" onPress={() => dispatch(addExample())} backgroundColor='red' textColor='white' />
             </Card>
-            <PokemonNoDetail />
-        </View>
+            <PokemonNoDetail
+                imageURL="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/279.png"
+                name="Pelipper"
+            />
+            <PokemonFullDetail
+                imageURL="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/279.png"
+                pokemon={{
+                    name: 'Pelipper',
+                    weight: 40,
+                    type: ['normal'],
+                    abilities: ['limber', 'imposter'],
+                }}
+            />
+            <PokemonBasicDetail
+                imageURL="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/279.png"
+                pokemon={{
+                    name: 'Pelipper',
+                    weight: 40,
+                    type: ['normal'],
+                }}
+            />
+        </ScrollView>
     );
 }
 
-const styles = StyleSheet.create({
-    
-});
+const styles = StyleSheet.create({});
