7b44fee3e7d9372487c9aaebf5c56474b189c459
asen23 <andersenlim23@gmail.com>
2021-10-26 11:34:57 +0700

    feat: add redux and navigation to app

--- App.js
+++ App.js
@@ -7,10 +7,32 @@
  */
 
 import React from 'react';
-import type {Node} from 'react';
-import {SafeAreaView} from 'react-native';
+import { Text } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { persistor, store } from './src/redux/store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-const App: () => Node = () => {
-  return <SafeAreaView></SafeAreaView>;
+const Stack = createNativeStackNavigator<StackParamList>();
+
+const Test = () => {
+    return <Text>Hello World!</Text>;
 };
-export default App;
+
+export default function App() {
+    return (
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <SafeAreaProvider>
+                    <NavigationContainer>
+                        <Stack.Navigator>
+                            <Stack.Screen name="Test" component={Test} />
+                        </Stack.Navigator>
+                    </NavigationContainer>
+                </SafeAreaProvider>
+            </PersistGate>
+        </Provider>
+    );
+}
