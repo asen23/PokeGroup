d3695a2ff31ce1433bebfe7688386996de67c91b
asen23 <andersenlim23@gmail.com>
2021-10-28 10:17:34 +0700

    feat: add data fetching logic to pokemonbasic

--- src/components/PokemonBasicDetail.js
+++ src/components/PokemonBasicDetail.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Button from './Button';
 import Card from './Card';
@@ -6,16 +6,46 @@ import CircleImage from './CircleImage';
 import DetailColumn from './DetailColumn';
 import TitleName from './TitleName';
 
-const PokemonBasicDetail = ({ imageURL, pokemon }) => {
+const PokemonBasicDetail = ({ id }) => {
+    const [pokemon, setPokemon] = useState({
+        name: '',
+        weight: '',
+        exp: '',
+        type: [],
+    });
+    const imageURL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+
+    const getPokemonData = async () => {
+        let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        let json = await response.json();
+        setPokemon({
+            name: json.species.name,
+            weight: json.weight,
+            exp: json.base_experience,
+            type: json.types.map(t => t.type.name),
+        });
+    };
+
+    useEffect(() => {
+        getPokemonData();
+    }, []);
     return (
         <Card style={styles.cardWrapper}>
             <CircleImage url={imageURL} />
             <View style={styles.content}>
                 <TitleName text={pokemon.name} style={styles.title} />
                 <DetailColumn>Weight: {pokemon.weight}</DetailColumn>
-                <DetailColumn>Base Exp: {pokemon.weight}</DetailColumn>
+                <DetailColumn>Base Exp: {pokemon.exp}</DetailColumn>
                 <DetailColumn>{pokemon.type}</DetailColumn>
-                <Button title="Evolution" textColor="blue"/>
+                <Button
+                    title="Evolution"
+                    textColor="blue"
+                    onPress={() =>
+                        navigation.navigate('Evolution', {
+                            evolutionURL: `https://pokeapi.co/api/v2/evolution-chain/${id}`,
+                        })
+                    }
+                />
             </View>
         </Card>
     );
